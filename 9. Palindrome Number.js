db1b402fbdd772428676be5402502327511de143
yangyang121 <yywq1314@gmail.com>
2017-03-31 14:30:31 +0800

    9 finish

--- 9. Palindrome Number.js
+++ 9. Palindrome Number.js
@@ -1,4 +1,20 @@
 var isPalindrome = function(x) {
+    if(x < 0){
+        return false
+    }else{
+        var sum = 0;
+        var a = x;
+        while(x){
+            sum = sum*10 + x%10;
+            x = parseInt(x/10);
+        }
+
+        if(sum===a){
+            return true
+        }else{
+            return false
+        }
+    }
     
 };
-isPalindrome();
+isPalindrome(232);
