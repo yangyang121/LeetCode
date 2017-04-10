d7bc5b6dfca4ea6c2947c0a471b3bb9c71780785
yangyang121 <yywq1314@gmail.com>
2017-04-10 16:54:57 +0800

    38 finish

---
+++ 38. Count and Say.js
@@ -0,0 +1,21 @@
+var countAndSay = function(n) {
+	//参考了别人的代码
+   	if(n === 0){
+   		return ""
+   	}
+   	var res = '1';
+   	while(--n){
+   		var cur = '';
+   		for(var i = 0;i<res.length;i++){
+   			var count = 1;
+   			while((i + 1 < res.length) && (res[i] === res[i + 1])){
+   				count++;
+   				i++;
+   			}
+   			cur += count + res[i];
+   		}
+   		res = cur;
+   	}
+   	console.log(res)
+};
+countAndSay(5)
