474c337aadfb34ec40e4ebc6553288db4f68225d
yangyang121 <yywq1314@gmail.com>
2017-04-16 20:17:25 +0800

    49 finish

---
+++ 49. Group Anagrams.js
@@ -0,0 +1,21 @@
+var groupAnagrams = function(strs) {
+    var strs = strs.sort()
+    var map = {}
+    var result = []
+    for(var i = 0;i < strs.length;i++){
+    	var str = strs[i]
+    	var sorted = str.split('').sort().join('')
+
+    	if(map[sorted] === undefined){
+    		map[sorted] = [str]
+    	}else{
+    		map[sorted].push(str)
+    	}
+    }
+    
+    for(var arr in map){
+    	result.push(map[arr])
+    }	
+    return result
+};
+groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
