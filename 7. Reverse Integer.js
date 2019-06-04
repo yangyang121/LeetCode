86614cdb51fa299332bc4f1d1cd870bcd2e0f3d2
yangyang <844171995@qq.com>
2019-06-04 20:20:23 +0800

    update:优化

--- 7. Reverse Integer.js
+++ 7. Reverse Integer.js
@@ -1,6 +1,7 @@
 // 数字范围是 [-2147483648, 2147483647]，此范围对输入x和输出的result同时适用
 var reverse = function(x) {
-  if (x >= 2147483647 || x <= -2147483648) return 0
+  const isLimited = num => num >= Math.pow(2, 31) - 1 || num <= Math.pow(-2, 31)
+  if (isLimited(x)) return 0
   const str = x + ""
   let a = ""
   let newStr = str
@@ -10,7 +11,7 @@ var reverse = function(x) {
   }
   const arr = newStr.split("")
   const result = parseFloat(a + arr.reverse().join(""))
-  if (result >= 2147483647 || result <= -2147483648) return 0
+  if (isLimited(result)) return 0
   return result
 }
 reverse(123) //321
