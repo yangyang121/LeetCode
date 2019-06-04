b44773e78e58687d1e6597a4072602744a16db25
yangyang <844171995@qq.com>
2019-06-04 20:06:16 +0800

    update:优化第七题

--- 7. Reverse Integer.js
+++ 7. Reverse Integer.js
@@ -1,23 +1,17 @@
+// 数字范围是 [-2147483648, 2147483647]，此范围对输入x和输出的result同时适用
 var reverse = function(x) {
-    if(x!==''){
-        if(x === 1534236469 || x === 1563847412 || x === -1563847412 || x === 2147483647 || x === -2147483648){
-            return 0
-        }else{
-            var str = x + '';
-        var a = '';
-        var newStr = str;
-        var s;
-        if(str[0]==='-'){
-            a = '-';
-            newStr = str.replace('-','');
-        }
-        var arr = newStr.split('');
-        s = parseFloat(a + arr.reverse().join(''));
-        console.log(s)
-        return s
-        }
-    }
-    
-};
-reverse(123);//321
-reverse(-123);//-321
+  if (x >= 2147483647 || x <= -2147483648) return 0
+  const str = x + ""
+  let a = ""
+  let newStr = str
+  if (str[0] === "-") {
+    a = "-"
+    newStr = str.replace("-", "")
+  }
+  const arr = newStr.split("")
+  const result = parseFloat(a + arr.reverse().join(""))
+  if (result >= 2147483647 || result <= -2147483648) return 0
+  return result
+}
+reverse(123) //321
+reverse(-123) //-321
