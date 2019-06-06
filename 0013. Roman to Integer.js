2554a15d4bdc55df7d978fcf2e5dedbfb3060c78
yangyang <844171995@qq.com>
2019-06-06 17:20:17 +0800

    update:finish 13

--- 0013. Roman to Integer.js
+++ 0013. Roman to Integer.js
@@ -1,16 +1,19 @@
 var romanToInt = function(s) {
-  var roman = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 }
-  var result = 0
+  const roman = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 }
+  let result = 0
 
-  for (var i = 0; i < s.length; i++) {
-    //console.log(roman[s[i]]);
-    if (roman[s[i]] < roman[s[i + 1]]) {
+  for (let i = 0; i < s.length; i++) {
+    if (s[i + 1] && roman[s[i]] < roman[s[i + 1]]) {
       result -= roman[s[i]]
     } else {
       result += roman[s[i]]
     }
   }
-  console.log(result)
+
   return result
 }
-romanToInt()
+console.log(romanToInt("III"))
+console.log(romanToInt("IV"))
+console.log(romanToInt("IX"))
+console.log(romanToInt("LVIII"))
+console.log(romanToInt("MCMXCIV"))
