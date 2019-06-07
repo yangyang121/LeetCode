0612ee9816ebd36ff8079efe0be89c1e7cf81970
yangyang <844171995@qq.com>
2019-06-07 20:23:11 +0800

    update: finish 0014

--- 0014. Longest Common Prefix.js
+++ 0014. Longest Common Prefix.js
@@ -1,2 +1,31 @@
-var longestCommonPrefix = function(strs) {}
-longestCommonPrefix(["", ""])
+var longestCommonPrefix = function(strs) {
+  if (!Array.isArray(strs) || strs.includes("")) return ""
+  if (strs.length === 0) {
+    return ""
+  } else if (strs.length === 1) {
+    return strs[0]
+  }
+  const item = strs[0]
+  const array = strs.slice(1)
+  let result = ""
+  let resIndex = 0
+  let allTrue = true
+  while (allTrue) {
+    if (!array.every(element => element.startsWith(result))) {
+      allTrue = false
+      result = result.slice(0, -1)
+      break
+    }
+    if (++resIndex > item.length) {
+      allTrue = false
+    } else {
+      result = item.slice(0, resIndex)
+    }
+  }
+  return result
+}
+console.log(longestCommonPrefix(["", ""]))
+console.log(longestCommonPrefix(["c", "c"]))
+console.log(longestCommonPrefix(["flower", "flow", "flight"]))
+console.log(longestCommonPrefix(["flower", "flow", "floweight"]))
+console.log(longestCommonPrefix(["dog", "racecar", "car"]))
---
+++ 0020. Valid Parentheses.js
@@ -0,0 +1,8 @@
+var isValid = function(s) {
+    
+}
+console.log(isValid('()'))
+console.log(isValid('()[]{}'))
+console.log(isValid('(]'))
+console.log(isValid('([)]'))
+console.log(isValid('{[]}'))
