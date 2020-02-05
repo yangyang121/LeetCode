f34e754f97412f1540dffdfa6911dc4b5686f2f8
yangyang <844171995@qq.com>
2020-02-05 20:15:20 +0800

    优化58

--- 0058. Length of Last Word.js
+++ 0058. Length of Last Word.js
@@ -3,16 +3,9 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-  if (!s.trim()) return 0
-  const arr = s.split(" ")
-  let result = 0
-  arr.reverse().forEach(item => {
-    console.log(item)
-    if (item && !resule) {
-      result = item.length
-    }
-  })
-  return result
+  let end = s.length - 1
+  while (end >= 0 && s[end] === " ") end--
+  let start = end
+  while (start >= 0 && s[start] !== " ") start--
+  return end - start
 }
-
-console.log(lengthOfLastWord("Today is a nice day"))
