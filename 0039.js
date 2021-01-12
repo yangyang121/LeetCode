45eb34fae7505a80ada4f90860a0c9fd3c0ce5e9
yangyang121 <844171995@qq.com>
2021-01-12 19:38:05 +0800

    update

--- 0039.js
+++ 0039.js
@@ -4,24 +4,23 @@
  * @return {number[][]}
  */
 var combinationSum = function (candidates, target) {
-  if (!candidates.length) return [];
-  const ans = [];
-  candidates.sort((a, b) => a - b);
+  if (!candidates.length) return []
+  const ans = []
   function dfs(path, t, start) {
     if (t <= 0) {
-      t === 0 && ans.push(path.slice());
-      return;
+      t === 0 && ans.push(path.slice())
+      return
     }
     for (let i = start; i < candidates.length; i++) {
-      path.push(candidates[i]);
-      dfs(path, t - candidates[i], i);
-      path.pop();
+      path.push(candidates[i])
+      dfs(path, t - candidates[i], i)
+      path.pop()
     }
   }
-  dfs([], target, 0);
-  console.log(ans);
-  return ans;
-};
+  dfs([], target, 0)
+  console.log(ans)
+  return ans
+}
 
-combinationSum([2, 3, 6, 7], 7);
-combinationSum([2, 3, 5], 8);
+combinationSum([2, 3, 6, 7], 7)
+combinationSum([2, 3, 5], 8)
--- 0040.js
+++ 0040.js
@@ -4,26 +4,26 @@
  * @return {number[][]}
  */
 var combinationSum2 = function (candidates, target) {
-  if (!candidates.length) return [];
-  const ans = [];
-  candidates.sort((a, b) => a - b);
+  if (!candidates.length) return []
+  const ans = []
+  candidates.sort((a, b) => a - b)
   function dfs(path, t, start) {
-    if (t <= 0) {
-      t === 0 && ans.push(path.slice());
-      return;
+    if (t === 0) {
+      ans.push(path.slice())
+      return
     }
     for (let i = start; i < candidates.length; i++) {
-      if (t - candidates < 0) break;
-      if (i > start && candidates[i - 1] === candidates[i]) continue;
-      path.push(candidates[i]);
-      dfs(path, t - candidates[i], i + 1);
-      path.pop();
+      if (t - candidates[i] < 0) break
+      if (i > start && candidates[i - 1] === candidates[i]) continue
+      path.push(candidates[i])
+      dfs(path, t - candidates[i], i + 1)
+      path.pop()
     }
   }
-  dfs([], target, 0);
-  console.log(ans);
-  return ans;
-};
+  dfs([], target, 0)
+  console.log(ans)
+  return ans
+}
 
-combinationSum2([10, 1, 2, 7, 6, 1, 5], 8);
-combinationSum2([2, 5, 2, 1, 2], 5);
+combinationSum2([10, 1, 2, 7, 6, 1, 5], 8)
+combinationSum2([2, 5, 2, 1, 2], 5)
