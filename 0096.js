fa65d47a8765b6b74da456c3dd11c5fedb192b02
yangyang121 <844171995@qq.com>
2021-01-29 21:29:29 +0800

    update

--- 0096.js
+++ 0096.js
@@ -3,9 +3,13 @@
  * @return {number}
  */
 var numTrees = function (n) {
-  let C = 1;
-  for (let i = 0; i < n; i++) {
-    C = (C * 2 * (2 * i + 1)) / (i + 2);
+  const dp = new Array(n + 1).fill(0)
+  dp[0] = 1
+  dp[1] = 1
+  for (let i = 2; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      dp[i] += dp[j - 1] * dp[i - j]
+    }
   }
-  return C;
-};
+  return dp[n]
+}
